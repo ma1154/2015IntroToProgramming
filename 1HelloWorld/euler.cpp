b27963d5eb8508719c066083a706350b42e6e878
ma1154 <tarboos1995@gmail.com>
2015-03-24 18:19:56 +0000

    HW 1 first commit

---
+++ 1HelloWorld/euler.cpp
@@ -0,0 +1,18 @@
+//HW1 Mohammed
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int total = 0;
+    for ( int i = 0; i < 1000; i++ ) {
+        if ( i % 3 == 0 || i % 5 == 0 ) {
+            printf( " %i ", i );
+            total = total + i;
+        }
+    }
+    printf( "\nTotal: %i\n", total );
+    return 0;
+}
